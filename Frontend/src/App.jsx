d258bab94da95e8b9d200bbed4ebc12a370fbda9
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-06 12:01:31 +0500

    frontend backend connect

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// vite config file me proxy add krdi.
 
+function App() {
+  const [fruits, setFruits] = useState(null)
+
+  useEffect(() => {
+    getFruits();
+  }, [])
+
+  const getFruits = async () => {
+    try {
+      const fruitsGet = await axios.get('/api/fruits');
+      setFruits(fruitsGet.data);
+      // console.log(fruitsGet.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  console.log("fruits", fruits);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Fruits</h2>
+      {
+        fruits.map((data) => {
+          return <ul style={{
+            listStyle: 'none'
+          }}>
+            <li>{data?.id}</li>
+            <li>{data?.name}</li>
+          </ul>
+        })
+      }
     </>
   )
 }
